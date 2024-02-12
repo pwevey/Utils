73a116918e721ef838aa836eb1879687b643e9ab
Renley <47496733+pwevey@users.noreply.github.com>
2024-02-12 09:32:44 -0500

    Added additional cases where extra post variable names was in other p tags

--- Python Scripts/parse_postvariables_webpage.py
+++ Python Scripts/parse_postvariables_webpage.py
@@ -24,7 +24,6 @@ for table in soup.find_all('table'):
         else:
             post_variable['postVariableName'] = anchor_name if anchor_name else None
 
-
         # Extract jobTypes and description from other tr tags
         job_types = []
         description = None
@@ -46,7 +45,7 @@ for table in soup.find_all('table'):
             if len(tds) >= 2:
                 # Description found in the second td
                 description = tds[1].get_text(strip=True)
-                
+
                 # Remove \u00a0 and \u00e2\u0080\u0093 from descriptions
                 description = description.replace('\u00a0', '')
                 post_variable['description'] = description.replace('\u00e2\u0080\u0093', '')
@@ -55,9 +54,6 @@ for table in soup.find_all('table'):
                 # There is a typo for this post var
                 if description == "Signals the output of the stock definition":
                     post_variable['postVariableName'] = "output_stock_definition"
-
-                # if post_variable['postVariableName'] == "arc_center":
-                #     print(description)
 
                 if "API:" in description or "TCPAPI" in description or re.search(r'\bAPI\b', description):
                     # Extract API calls from the description
@@ -68,24 +64,20 @@ for table in soup.find_all('table'):
                         if not any(char.isdigit() for char in api_tuple)
                         for api in api_tuple
                     ]
-                    
-                    # print(api_calls)
 
                     # Remove single-word entries
                     api_calls = [api for api in api_calls if not api.lower() in ["get", "lathe", "mill"]]
 
                     # Remove instances of '\u00e2\u0080\u009c' and '\u00e2\u0080\u009d' from each API call
-                    api_calls = [api.replace('\u00e2\u0080\u009c', '').replace('\u00e2\u0080\u009d', '') for api in api_calls]            
+                    api_calls = [api.replace('\u00e2\u0080\u009c', '').replace('\u00e2\u0080\u009d', '') for api in api_calls]
 
+        # Finds and records the extra API calls in the third row of the table
         try:
             extra_apis_row = table.find_all('tr')[2]
             if extra_apis_row:
                 tds_extra = extra_apis_row.find_all('td')
                 if len(tds_extra) >= 2:
                     description_extra = tds_extra[1].get_text(strip=True)
-
-                    # if post_variable['postVariableName'] == "arc_center":
-                    #     print(description_extra)
 
                     if "API:" in description_extra or "TCPAPI" in description_extra or re.search(r'\bAPI\b', description_extra):
                         # Extract API calls from the description
@@ -105,15 +97,26 @@ for table in soup.find_all('table'):
 
                         api_calls += api_calls_extra
 
-
         except IndexError:
             pass
-
-        # print(api_calls)
 
         post_variable['bobcadAPIs'] = api_calls
         post_variables.append(post_variable)
 
+    # Cases where there are multiple p tags in one table for each post variable
+    first_td = table.select_one('tr:first-child td:first-child')
+    while first_td:
+
+        for index, p_tag in enumerate(first_td.find_all('p')):
+            if index > 0:
+                # Get any additional post variable names
+                extra_var = p_tag.get_text(strip=True)
+                if extra_var:
+                    new_entry = post_variable.copy()  # Copy the existing entry
+                    new_entry['postVariableName'] = extra_var  # Replace postVariableName with the new one
+                    post_variables.append(new_entry)
+
+        break
 
 # Create a folder named "data" if it doesn't exist
 data_folder = 'data'
